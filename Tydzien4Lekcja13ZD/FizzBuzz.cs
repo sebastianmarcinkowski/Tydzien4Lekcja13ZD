6778e65be2b6423aba79aa262147e07f15dc2365
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-07-18 19:58:13 +0200

    Entry version FizzBuzz Game.

---
+++ Tydzien4Lekcja13ZD/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿namespace Tydzien4Lekcja13ZD
+{
+	internal class FizzBuzz
+	{
+		private int _number;
+		public FizzBuzz(int number)
+		{
+			_number = number;
+		}
+		public string Play()
+		{
+			if ((_number % 3 == 0) && (_number % 5 == 0)) return "FizzBuzz";
+			if (_number % 3 == 0) return "Fizz";
+			if (_number % 5 == 0) return "Buzz";
+			return _number.ToString();
+		}
+	}
+}
--- Tydzien4Lekcja13ZD/Program.cs
+++ Tydzien4Lekcja13ZD/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tydzien4Lekcja13ZD
 {
@@ -10,6 +6,62 @@ namespace Tydzien4Lekcja13ZD
 	{
 		static void Main(string[] args)
 		{
+			try
+			{
+				while (true)
+				{
+					Description();
+
+					var number = GetInt();
+					Console.Clear();
+
+					var game = new FizzBuzz(number);
+					Console.WriteLine($"Teraz moja kolej... {game.Play()}!");
+
+					Console.Write("Aby sprawdzić koleją liczbę wpisz 'TAK' i zatwierdź enterem, w przeciwnym wypadku wystarczy wcisnąć samo enter: ");
+					if (!(Console.ReadLine().ToUpper() == "TAK"))
+						break;
+					Console.Clear();
+				}
+			}
+			catch (Exception)
+			{
+				Console.Clear();
+				Console.WriteLine("Wystąpił nieoczekiwany błąd, program zostanie zamknięty!");
+			}
+			finally
+			{
+				Console.Write("Aby zakończyć program naciśnij dowolny klawisz...");
+				Console.ReadKey();
+			}
+		}
+		static void Description()
+		{
+			Console.WriteLine("Zagrajmy w FizzBuzz!");
+			Console.WriteLine("---------------------------------------------------------------------------------");
+			Console.WriteLine("Jeśli podana przez Ciebie liczba będzie podzielna przez 3 wyświetlę Fizz.");
+			Console.WriteLine("Jeśli podana przez Ciebie liczba będzie podzielna przez 5 wyświetlę Buzz.");
+			Console.WriteLine("Jeśli podana przez Ciebie liczba będzie podzielna przez 3 i 5 wyświetlę FizzBuzz!");
+			Console.WriteLine("W innym przypadku wyświetlę liczbę którą wprowadziłeś.");
+			Console.WriteLine("---------------------------------------------------------------------------------");
+		}
+		static int GetInt()
+		{
+			var number = 0;
+
+			Console.Write("Wprowadź liczbę: ");
+
+			while (true)
+			{
+				if (int.TryParse(Console.ReadLine(), out number))
+					break;
+
+				Console.Clear();
+				Console.WriteLine("Podana wartość jest nieprawidłowa, spróbuj jeszcze raz!");
+				Console.Write("Wprowadź liczbę: ");
+			}
+
+			return number;
 		}
 	}
 }
